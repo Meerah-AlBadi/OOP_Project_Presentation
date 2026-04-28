d024a58ea0d4c09f7c486916cc04e7c110a19133
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-04-28 19:30:41 +0400

    Added inventory attributes

--- Inventory.h
+++ Inventory.h
@@ -1,4 +1,9 @@
-Product products[100]; //array stores all products in inventory
-int quantities[100]; //array stores each prudcts quantity
-int count; //checks the number of products in inventroy
+//Inventory class
+class Inventory {
+private:
+    Product products[100]; //array stores all products in inventory
+    int quantities[100]; //array stores each products quantity
+    int count; //checks the number of products in inventory
+public:
 
+};
