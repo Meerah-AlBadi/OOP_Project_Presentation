445a989e6d8ed0ce38f926e46bbc7e3ff46e73ba
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-04-29 09:12:54 +0400

    improved formatting

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 int main() {
     Inventory inventory; //Inventory object
     int qty,id;
+    //input of Product ID
     cout<<"Enter the ID of the product: ";
     cin>>id;
+
     cout<<"Enter quantity:";
     cin>>qty;
     if (qty<=0) {
         cout<<"Invalid quantity";
         return 0;
     }
+    //creating a product object
     Product p(id); //temporary until Product class is created
     inventory.add_product(p,qty);
     int amount_purchase;
@@ -23,7 +26,7 @@ int main() {
         cout<<"Invalid purchase amount";
         return 0;
     }
-    inventory.reduce_stock(id,amount_purchase);
+    inventory.reduce_stock(id,amount_purchase)
     inventory.display_inventory();
     return 0;
 }
