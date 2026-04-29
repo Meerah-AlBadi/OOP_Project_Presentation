cd8767ad8d039bb0577eb51694a0b9ede47527c1
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-04-29 08:46:21 +0400

    draft for testing will be changed once Product class is added

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+//temporary draft only for testing purposes
+#include "Inventory.h"
+#include <iostream>
+using namespace std;
 
+int main() {
+    Inventory inventory; //Inventory object
+    int qty,id;
+    cout<<"Enter the ID of the product:";
+    cin>>id;
+    cout<<"Enter quantity:";
+    cin>>qty;
+    Product p(id); //temporary until Product class is created
+    inventory.add_product(p,qty);
+    int amount_purchase;
+    cout<<"Enter amount to purchase:";
+    cin>>amount_purchase;
+    inventory.reduce_stock(id,amount_purchase);
+    inventory.display_inventory();
+    return 0
+}
