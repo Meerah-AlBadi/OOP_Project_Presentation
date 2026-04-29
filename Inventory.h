696454e452408922cfb938d4ebbd6cf308f0d3b6
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-04-29 06:16:30 +0400

    Completed Inventory.h code

--- Inventory.h
+++ Inventory.h
@@ -1,10 +1,10 @@
 //Inventory class
 class Inventory {
 private:
-    Product products[100]; //array stores all products in inventory
-    int quantities[100]; //array stores each products quantity
+    Product products[100]; //stores all products in inventory
+    int quantities[100]; //stores each products quantity
     int count; //checks the number of products in inventory
 public:
-    Inventory();
-    void addProduct(Product prdct, int qty);
+    Inventory(); //constructor
+    void add_product(Product prdct, int qty); //function
 };
