8c9d80e24b617c44111855b226a90d18b986a6b5
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-04-29 06:29:08 +0400

    Implemented add_product function

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,1 +1,10 @@
 #include "Inventory.h"
+
+Inventory::Inventory() {
+    count=0;
+}
+void Inventory::add_product(Product prdct, int qty) {
+    products[count]=prdct;
+    quantities[count]=qty;
+    count++;
+}
