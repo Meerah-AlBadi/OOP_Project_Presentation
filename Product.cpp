011b11813550bdce5499ecb7006bd48069a4b417
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-04-30 13:43:43 +0400

    Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -2,26 +2,27 @@
 #include <string>
 #include "Product.h"
 using namespace std;
-// Product class (.cpp)
 
-Product() {    //Initializer Constructor
+Product::Product() {    //Initializer Constructor
     ProductID =0;
     Product_Name="";
     Price =0;
 }
-Product(int id,string name,double p): ProductID(id),Product_Name(name),Price(p) {}//Paramenterized Constructor
+Product::Product(int id,string name,double p): ProductID(id),Product_Name(name),Price(p) {}//Paramenterized Constructor
 
 
-int getProductID() {return ProductID;}   //returns Product id
-string getProductName() {return Product_Name}   // returns Product Name
-void setPrice(double p) {   // set the price of the product
+int Product::getProductID() {return ProductID;}   //returns Product id
+
+string Product::getProductName() {return Product_Name;}   // returns Product Name
+
+void Product::setPrice(double p) {   // set the price of the product
     if (p>=0) {
         Price =p;
     }
 }
 
-void displayProductinfo() {              // prints Product
-    cout << "Product ID: "<<getProductID()<<endl;
-    cout << "Product Name: "<<getProductName()<<endl;
-    cout << "Product Price: "<<Price<<"OMR "<<endl;
+void Product::displayProduct_info() {  // print Product information
+    cout <<"Product ID: "<<getProductID() <<endl;
+    cout << "Product Name: "<< getProductName() <<endl;
+    cout << "Product Price: " << Price << "OMR "<<endl;
 }
