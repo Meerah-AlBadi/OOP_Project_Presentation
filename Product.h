b317c83e0d263cd00adc3c03489e11090e19b2a2
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-04-30 13:10:53 +0400

    Product.h

--- Product.h
+++ Product.h
@@ -1,1 +1,13 @@
+// Product class (.h)
+class Product {
+    int ProductID;
+    string Product_Name;
+    double Price;
+public:
+    Product();   //Initializer Constructor
+    Product(int id,string name,double p);  //Paramenterized Constructor
+    int getProductID();   //returns Product id
+    string getProductName();   // returns Product Name
+    void setPrice(double p);   // set the price of the product
+};
 
