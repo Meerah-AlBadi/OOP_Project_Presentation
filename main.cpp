f4951d30151b14f770d2229a24aa64aca8073421
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-07 22:26:53 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,3 +30,149 @@ int main() {
     inventory.display_inventory();
     return 0;
 }
+
+
+// ----------------------MAIN--------------------------
+//  #include <iostream>
+//#include <string>
+//  using namespace std;
+//
+//#include "User.h"
+//#include "Admin.h"
+//#include "Customer.h"
+//#include "Inventory.h"
+//#include "Product.h"
+//#include "Order.h"
+//#include "Payment.h"
+//  int main() {
+//      Inventory inventory; // Inventory object
+//      int option=-1;
+//      while (option !=0) {
+//          cout << "==== Gaming Store Management System ===="<<endl;
+//          cout << "1. Admin" << endl;
+//          cout << "2. Customer" << endl;
+//          cout << "0. Exit" << endl;
+//          cout <<  "Enter Your Chosen Option: ";
+//          cin >> option;
+//
+//          if (cin.fail()) {        // Checks if input failed
+//              cin.clear();        //  Removes the error state so cin works again
+//              cin.ignore(1000,'\n');    // Deletes the wrong input from memory
+//              cout << "Invalid input!!" << endl << "Please enter a number." << endl;  // Display an error message
+//              continue;
+//          }
+//          switch (option) {
+//              case 1: {
+//                  int AdminID;
+//                  string Admin_Name;
+//
+//                  cout << "Enter Admin ID: ";
+//                  cin >> AdminID;
+//
+//                  cout << "Enter Admin Name: ";
+//                  cin.ignore();
+//                  getline(cin,Admin_Name);
+//
+//                  Admin admin(AdminID, Admin_Name, &inventory);
+//
+//                  cout << endl<<"--- Admin Information ---" << endl;
+//                  admin.displayUser();
+//
+//                  int Admin_Option=-1;
+//
+//                  while (Admin_Option !=0) {
+//                      cout << "==== Admin Menu ====" << endl;
+//                      cout << "1.Add Product"<< endl;
+//                      cout << "2.Remove Product" << endl;
+//                      cout << "3.View Inventory"<< endl;
+//                      cout << "4.Update Product Stock" <<endl;
+//                      cout << "0.Back" << endl;
+//                      cout << "Enter Your Chosen Option: ";
+//                      cin >> Admin_Option;
+//
+//                      if (cin.fail()) {
+//                          cin.clear();
+//                          cin.ignore(1000,'\n');
+//                          cout << "Invalid input!! Please enter a number.";
+//                          continue;
+//                      }
+//
+//                      switch (Admin_Option) {
+//                          case 1: {
+//                              admin.Add_Product();
+//                              break;
+//                          }
+//                          case 2:
+//                              admin.Remove_Product();
+//                              break;
+//                          case 3:
+//                              admin.View_Inventory();
+//                              break;
+//                          case 4:
+//                              admin.Update_Product_Stock();
+//                              break;
+//                          case 0:
+//                              cout << "Returning to main menu"<<endl;
+//                              break;
+//                          default:
+//                              cout << "Invalid Choice! Try Again" << endl;
+//                      }
+//                  }
+//                  break;
+//          }
+//
+//              case 2: {
+//                  int CustomerID;
+//                  string Customer_Name;
+//
+//                  cout << "Enter Customer ID: ";
+//                  cin >> CustomerID;
+//
+//                  cout << "Enter Customer Name: ";
+//                  cin.ignore();
+//                  getline(cin,Customer_Name);
+//                  cin >> Customer_Name;
+//
+//                  Customer customer(CustomerID, Customer_Name);
+//                  cout << "--- Customer Information ---" << endl;
+//                  customer.displayUser();
+//
+//                  int orderID, productID,quantity;
+//
+//                  cout << "Enter Order ID: ";
+//                  cin >> orderID;
+//
+//                  cout << "Enter Product ID: ";
+//                  cin >> productID;
+//
+//                  cout << "Enter Quantity:";
+//                  cin >> quantity;
+//
+//                  Product* SelectedProduct = inventory.findProduct(productID);
+//
+//                  inventory.reduce_stock(productID, quantity);
+//
+//                  Order order(orderID, customer, SelectedProduct,quantity);
+//                  order.displayOrder();
+//
+//                  string PaymentMethod;
+//
+//                  cout << "Enter Payment Method: ";
+//                  cin  >> PaymentMethod;
+//
+//                  Payment payment(order.getProductID(), order.getProdutID(), order.getQuantity());
+//
+//                  cout << "Order Completed! Inventory updated successfully" <<endl;
+//                  break;
+//              }
+//              case 0: {
+//                  cout << "System Closed!" << endl;
+//                  break;
+//              }
+//              default:
+//                  cout << "Invalid option!! Try Again" << endl;
+//
+//          }
+//          return 0;
+//      }
+//  }
