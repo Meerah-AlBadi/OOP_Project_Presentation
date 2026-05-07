f53672e749f1845821a13634975bacfe2638bc6e
Monaalbalushi <munaalbalushia97@gmail.com>
2026-05-07 14:01:17 +0400

    Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,1 +1,13 @@
+#include "Customer.h"
+#include <iostream>
+using namespace std;
 
+Customer::Customer(): User(){}  // call default constructor of user class
+
+Customer::Customer(int id, string name):User(id, name){}
+// parameterized  constructor it sends id and name to user contsructor
+
+void Customer::displayCustomer() { // function to display customer information
+    cout<<"==== Customer details ===="<<endl ; //print title
+    displayUser(); //call function from user class
+}
