7fa0f5b016c34a44939907a38ff8a7cbf2aed00e
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-07 22:18:05 +0400

    Admin.h

--- Admin.h
+++ Admin.h
@@ -1,1 +1,29 @@
+// Admin Class 
+#ifndef ADMIN_H
+#define ADMIN_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Including necessary classes
+#include "User.h"
+#include "Inventory.h"
+#include "Product.h"
+
+class Admin: public User {   // Admin class is inherited from class User
+        Inventory* inventory;   //Pointer to inventory object
+public:
+    Admin();  // Initializer Constructor
+    Admin (int id, string name, Inventory* inv);  //Paramentarized Constructor
+
+    void Add_Product();  // Add product to inventory
+
+    void View_Inventory();   // view products on inventory
+
+    void Update_Product_Stock();   //update a product stock in inventory
+
+    void Remove_Product();    // remove a product from the inventory
+};
+
+#endif
