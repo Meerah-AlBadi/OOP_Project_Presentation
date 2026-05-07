3c77e1254f8995b5c3da5fa1fef2fb18a4de84e8
Monaalbalushi <munaalbalushia97@gmail.com>
2026-05-07 13:18:32 +0400

    User.h

--- User.h
+++ User.h
@@ -1,1 +1,18 @@
+// Class User
+#ifndef USER_h
+#define USER_h               
+#include<string>
+using namespace std;
 
+class User {
+protected:
+    int id;        // user id
+    string name;  //user name
+    public:
+    User() ;// initialized  constructor
+    User(int i, string n) ; //paramenterized constructor
+    int getId() ;// return user id
+    string getName() ; // return user name
+    void display() ; // display user info
+};
+#endif
