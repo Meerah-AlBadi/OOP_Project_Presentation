136fe3e26022a600e50faf7d3316af7ca38f717c
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-08 18:44:33 +0400

    Update Product.h

--- Product.h
+++ Product.h
@@ -9,7 +9,7 @@ class Product {
     double Price;
 public:
     Product();   //Initializer Constructor
-    Product(int id,string name,double p);  //Paramenterized Constructor
+    Product(int id,string name,double p);  //parameterized Constructor
     int getProductID();   //returns Product id
     string getProductName();   // returns Product Name
     void setPrice(double p);   // set the price of the product
