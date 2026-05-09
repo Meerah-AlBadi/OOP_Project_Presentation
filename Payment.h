63880e4c62cf9ebd34a09ae261c069c43a63961c
Maryam <urmemo2311@gmail.com>
2026-05-09 15:22:42 +0400

    Update Payment.h

--- Payment.h
+++ Payment.h
@@ -1,1 +1,16 @@
-
+#ifndef PAYMENT_H
+#define PAYMENT_H
+#include <iostream>
+using namespace std;
+class Payment{
+private:
+int idPayment;
+string methodPayment;
+double amount;
+public:
+Payment();
+Payment(int id, string method, double a);
+void displayPayment();
+void processPayment();
+};
+#endif
