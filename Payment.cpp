69a6870630b022c2d72c0f00d8e9ea1748a5119c
Maryam <urmemo2311@gmail.com>
2026-05-09 15:35:12 +0400

    Update Payment.cpp

--- Payment.cpp
+++ Payment.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "Payment.h"
+using namespace std;
+Payment::Payment(){
+  idPayment=0;
+  methodPayment="idk";
+  amount=0;
+}
+Payment::Payment(int id, string method, double a)
+: idPayment(id), methodPayment(method), amount(a){}
 
+void Payment::displayPayment(){
+  cout<<"Payment ID: "<<idPayment<<endl;
+  cout <<"Payment Method: "<<methodPayment<<endl;
+  cout<< "Amount: OMR" <<amount<<endl;
+}
