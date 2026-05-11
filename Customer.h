a91b5c74656f8b1797dddeadce18ef1a87211de1
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-11 19:59:13 +0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -4,11 +4,13 @@
 using namespace std;
 // customer class inherits from parent class(user class)
 class Customer: public User {
+    int CustomerID;
+    string CustomerName;;
 public:
     Customer(); // defualt constructor
 
     Customer(int id, string name);//parametrized constructor
-
+    string gerCusName();    // return Customer name
     void displayCustomer(); //display  customer details
 };
 #endif
