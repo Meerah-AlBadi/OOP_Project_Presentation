5266c6642abe8863b85e37c6a50632ddada0f3e1
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-11 20:04:54 +0400

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -7,9 +7,6 @@ Customer::Customer(): User(){}  // call default constructor of user class
 Customer::Customer(int id, string name):User(id, name){}
 // parameterized  constructor it sends id and name to user contsructor
 
-Customer :: getCusName() {
-        return name;
-}
 void Customer::displayCustomer() { // function to display customer information
     cout<<"==== Customer details ===="<<endl ; //print title
     displayUser(); //call function from user class
