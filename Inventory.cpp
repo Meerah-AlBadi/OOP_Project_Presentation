6539e7e929f2564e9e1e940e1ca6f40d4e6a9d5f
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-05-12 12:44:19 +0400

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -43,3 +43,48 @@ void Inventory::display_inventory() {
         cout<<products[i].getProductID()<<" "<<quantities[i]<<endl;
     }
 }
+// Function to findProduct
+Product* Inventory::findProduct (int ProductID) {
+    for (int i=0; i<size ;i++) {
+        if (Product[i].getProductID() == ProductID) {
+            return &Product[i];
+        }
+    }
+
+    return nullptr;
+}
+
+// Function to update Stock of a product
+bool Inventory::updateStock(int productID, int newQuantity) {
+    if (newQuantity < 0) {
+        cout<<"Invalid stock quantity"<<endl;
+        return false;
+    }
+    for (int i=0; i<size ; i++) {
+        if (products[i].getProductId() == productID) {
+            quantities[i] = newQuantity;
+            cout<<"Stock updates successfully"<<endl;
+            return true;
+        }
+    }
+    cout<<"product not found"<<endl;
+    return false;
+}
+
+// Remove a product
+bool Inventory::removeProduct(int productID) {
+    for (int i=0; i<size ; i++) {
+        if (product[i].getProductID()== productID) {
+            for (int j=i;j<size-1 ; j++) {
+                products[j]= products[j+1];
+                quantities[j]=quantities[j+1];
+            }
+            size--;
+            cout<<"product removed successfully"<<endl;
+            return true;
+        }
+    }
+    cout<<"product not found"<<endl;
+    return false;
+}
+
