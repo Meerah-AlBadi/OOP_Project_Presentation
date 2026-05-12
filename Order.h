725f53df8f8a34ee540d5c80023cf51bdcc64531
Monaalbalushi <munaalbalushia97@gmail.com>
2026-05-12 10:36:38 +0400

    Order.h

--- Order.h
+++ Order.h
@@ -1,1 +1,20 @@
+#indef ORDER_H
+#define ORDER_H
+#include <iostream>
+#include "customer.h"
+#include "product.h"
+using namespace std;
 
+class Order {
+int OrderID;
+Customer* customer;
+Product* product;
+Payment payment;
+int quantity;
+int total;
+public:
+Order();   // Initializer Constructor
+Order(int id, Customer* c, Product* p, int quan, Payment pay);    //Parameterized Counstructor
+void displayOrder();    // Print Order details
+};
+#endif
