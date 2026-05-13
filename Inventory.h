871679d038ce3caae8a25b669a01acfb20e0c583
Ibtihal-Almusallami <24-0272@student.gutech.edu.om>
2026-05-13 15:49:14 +0400

    Update Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -5,18 +5,16 @@
 #include "Product.h"
 #include <vector>
 
-using namespace std;
-
 class Inventory {
 private:
-    vector<Product> products; //stores all products in inventory
-	vector<int> quantities; //stores each products quantity
+    std::vector<Product> products; //stores all products in inventory
+	std::vector<int> quantities; // stores quantity of each product
 public:
     Inventory(); //constructor
 	~Inventory(); //destructor
     void add_product(Product prdct, int qty); //adds product function
     void reduce_stock(int id,int amount); //reduce stock function
-    void display_inventory(); //displays inventory funtion
+    void display_inventory(); //displays inventory function
     Product* findProduct(int ProductID); //find product declaration
 	bool updateStock(int productID, int newQuantity); //update stock declaration
 	bool removeProduct(int productID); //remove product declaration
