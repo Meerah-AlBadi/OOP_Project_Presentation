3a91f5aa2c932bea3cc789cd840a40c47272fd1e
Maryam <urmemo2311@gmail.com>
2026-05-16 14:47:51 +0400

    Update Cash.cpp

--- Cash.cpp
+++ Cash.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "Cash.h"
+using namespace std;
+class CashPayment : public Payment {
+public:
+    CashPayment(double amount) : Payment(amount) {}
 
+    bool processPayment() override {
+        cout << "Cash payment of " << fixed << setprecision(2) << amount << " OMR completed.\n";
+        paid = true;
+        return true;
+    }
+
+    void displayPaymentMethod() const override {
+        cout << "Cash Payment";
+    }
+};
