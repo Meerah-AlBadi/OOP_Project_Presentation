1b839ef3ed2cc9e314514a97f7f4f2f7c5c28243
Maryam <urmemo2311@gmail.com>
2026-05-16 15:00:02 +0400

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,2 +1,30 @@
 #ifndef CARD_H
 #define CARD_H
+#include <iostream>
+#include<iomanip>
+#include <string>
+using namespace std;
+
+class Payment{
+protected:
+double amont;
+bool paid;
+public:
+Payment(double amount): amount(amount),paid(false){}
+virtual bool processPayment() = 0; 
+virtual void displayPaymentMethod() const = 0;
+
+bool isPaid() const { return paid; }
+double getAmount() const { return amount; }
+virtual ~Payment() {}
+};
+
+class CardPayment : public Payment {
+private:
+string cardNumber;
+public:
+    CardPayment(double amount, string cardNumber);
+    bool processPayment() override;
+    void displayPaymentMethod() const override;
+};
+#endif
