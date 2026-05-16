a07b7e5d747b63d49a65b037aef246b1f0902391
Maryam <urmemo2311@gmail.com>
2026-05-16 14:44:34 +0400

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class CardPayment : public Payment {
+private:
+    string cardNumber;
+
+public:
+    CardPayment(double amount, string cardNumber) : Payment(amount), cardNumber(cardNumber) {}
+
+    bool processPayment() override {
+        if (cardNumber.length() < 4) {
+            cout << "Invalid card number.\n";
+            paid = false;
+            return false;
+        }
+        cout << "Card payment of " << fixed << setprecision(2) << amount << " OMR completed.\n";
+        paid = true;
+        return true;
+    }
+
+    void displayPaymentMethod() const override {
+        cout << "Card Payment";
+    }
+};
