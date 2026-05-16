6c6c003b3c1611fef3ed1af06888f889371dfb19
Maryam <urmemo2311@gmail.com>
2026-05-16 14:43:31 +0400

    Update Payment.cpp

--- Payment.cpp
+++ Payment.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include "Payment.h"
 using namespace std;
-Payment::Payment(){
-  idPayment=0;
-  methodPayment="";
-  amount=0;
-}
-Payment::Payment(int id, string method, double a)
-: idPayment(id), methodPayment(method), amount(a){}
+class Payment {
+protected:
+    double amount;
+    bool paid;
+public:
+    Payment(double amount) : amount(amount), paid(false) {}
 
-void Payment::displayPayment(){
-  cout << endl;
-  cout << "--- Payment Details ---"<<endl;
-  cout<<"Payment ID: "<<idPayment<<endl;
-  cout <<"Payment Method: "<<methodPayment<<endl;
-  cout<< "Amount: OMR" <<amount<<endl;
-}
+    virtual bool processPayment() = 0;
+    virtual void displayPaymentMethod() const = 0;
+
+    bool isPaid() const {
+        return paid;
+    }
+    virtual ~Payment() {}
+};
