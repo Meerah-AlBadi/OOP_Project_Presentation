59b8573676f5441d9d47e0e89751984eab2ae32d
Monaalbalushi <munaalbalushia97@gmail.com>
2026-05-16 21:37:16 +0400

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,13 +1,39 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
-#include "User.h"
-using namespace std;
-// customer class inherits from parent class(user class)
-class Customer: public User {
-public:
-    Customer(); // defualt constructor
 
-    Customer(int id, string name);//parametrized constructor
-    void displayCustomer(); //display  customer details
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include "User.h"
+#include "Order.h"
+#include "Inventory.h"
+#include "Product.h"
+#include "CashPayment.h"
+#include "CardPayment.h"
+
+using namespace std;
+
+class Customer : public User {
+private:
+    static int customerCounter;
+    vector<Order*> orders;
+
+public:
+    // Constructors
+    Customer();
+    Customer(string name, string password);
+
+    // Override function
+    void showMenu() override;
+
+    // Functions
+    void viewProducts(const Inventory& inventory) const;
+    void placeOrder(Inventory& inventory);
+    void viewOrderHistory() const;
+
+    // Destructor
+    ~Customer();
 };
+
 #endif
