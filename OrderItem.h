90af71957fc746c45209032a57b7cca4e74fff6f
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-18 00:53:30 +0400

    Update OrderItem.h

--- OrderItem.h
+++ OrderItem.h
@@ -1,1 +1,24 @@
+#ifndef ORDERITEM_H
+#define ORDERITEM_H
 
+#include "Product.h"
+#include <string>
+using namespace std;
+
+
+class OrderItem {
+    product product;       // Composition (OrderItem HAS-A Product )
+    int quantity;
+public:
+    OrderItem(Product pr, int quan);     //Constructor (Takes a product object + quantity )
+
+    string getProductID() const;    // return Product Id (calle from the Product class)
+
+    int getQuantity() const;      // return the quantity of an ordered Product
+
+    double getSubtotal() const;     //Calculates the total price using quamtity of one product
+
+    void display() const;    // Shows information of one tyoe of ordered product
+};
+
+#endif
