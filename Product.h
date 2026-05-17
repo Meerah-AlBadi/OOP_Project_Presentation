69f7753522a433b6d2f6d53f2756b6615a675730
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-18 00:03:20 +0400

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,20 +1,35 @@
 #ifndef PRODUCT_H
 #define PRODUCT_H
+
 #include <string>
+#include <sstream>
+
 using namespace std;
 // Product class (.h)
 class Product {
-    int ProductID;
-    string Product_Name;
-    double Price;
+    static int ProductCounter;
+    string productID, productName, companyName;
+    double price;
 public:
-    Product();   //Initializer Constructor
-    Product(int id,string name,double p);  //parameterized Constructor
-    int getProductID();   //returns Product id
-    string getProductName();   // returns Product Name
-    void setPrice(double p);   // set the price of the product
-    double getPrice();          // returns price of Product
-    void displayProduct_info();  // prints product information (id,name,price)
-};
+    Product();                     // Default Constructor
 
+    Product(string pr_name, string co_name, double pr) ;     // Parametarized Constructor
+
+    string getProductID();      // returns Product id
+
+    string getProductName();    // returns Product Name
+
+    string getCompanyName();    // returns Product's Campany name
+
+    double getPrice();          // return Product's Price
+
+    void setProductName(string pr_name);   //  a setter method to set Name of product
+
+    bool setPrice(double pr);     //  a setter method (bool because it checks if price is valid otherwise it returns false )
+
+    void display() const;     // Prints Product Information
+
+    bool operator<(const Product& obj) const;     // Operator Overloading which is used to compare products by price
+    
+};
 #endif
