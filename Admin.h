d173d1e9325e136ad2609eb4d021cc36dd084f75
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-17 23:12:31 +0400

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -12,19 +12,18 @@ using namespace std;
 #include "Product.h"
 
 class Admin: public User {   // Admin class is inherited from class User
+        static int adminCounter;
         Inventory* inventory;   //Pointer to inventory object
 public:
     Admin();  // Initializer Constructor
     Admin (int id, string name, Inventory* inv);  //parameterized  Constructor
 
-    void Add_Product();  // Add product to inventory
-
-    void View_Inventory();   // view products on inventory
-
-    void Update_Product_Stock();   //update a product stock in inventory
-
-    void Remove_Product();    // remove a product from the inventory
-    void Update_Product_Price();
+    void showMenu() override;
+    void addProduct();  // Add product to inventory
+    void viewInventory() const;   // view products on inventory
+    void updateProductStock();   //update a product stock in inventory
+    void updateProductPrice();    // remove a product from the inventory
+    void updateProductPrice();
 };
 
 #endif
