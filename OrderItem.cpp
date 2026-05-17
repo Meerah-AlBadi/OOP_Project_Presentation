ada434c51e0a43cb5bca25e481f0538184778b3e
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-18 00:54:13 +0400

    Update OrderItem.cpp

--- OrderItem.cpp
+++ OrderItem.cpp
@@ -1,1 +1,34 @@
+#include "OrderItem.h"
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+OrderItem::OrderItem(Product pr, int quan) {
+
+    product= pr;
+
+    if (quan <0) {
+        quantity =0;
+    }
+    else
+        quantity = quan;
+
+}
+
+string OrderItem::getProductID() const {
+    return product.getProductID();
+}
+
+int OrderItem::getQuantity() const {
+    return quantity;
+}
+
+double OrderItem::getSubtotal() const {
+    return product.getPrice()*quantity;
+}
+
+void OrderItem::display() const {
+    product.display();
+    cout << setw(10) << quantity << setw(10) << getSubtotal() << endl;
+}
