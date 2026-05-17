1e5fd36b2f4e0212a10afccfbb58293cb312d403
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-18 00:03:47 +0400

    Update Product.cpp

--- Product.cpp
+++ Product.cpp
@@ -1,32 +1,66 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 #include "Product.h"
-using namespace std;
 
-Product::Product() {    //Initializer Constructor
-    ProductID =0;
-    Product_Name="";
-    Price =0;
+// Initialize static variable
+int Product::ProductCounter = 1;
+
+Product::Product() {
+    productID ="";
+    productName = "";
+    companyName ="";
+    price =0.0;
 }
-Product::Product(int id,string name,double p): ProductID(id),Product_Name(name),Price(p) {}//parameterized  Constructor
+
+Product::Product(string pr_name, string co_name, double pr) {
+
+    // Automatic Id creation
+    stringstream ss;
+    ss << "P" << ProductCounter++;
+    productID = ss.str();
+
+    productName = pr_name;
+    companyName = co_name;
+    price = pr;
+}
 
 
-int Product::getProductID() {return ProductID;}   //returns Product id
+string Product::getProductID() {
+    return productID;
+}
 
-string Product::getProductName() {return Product_Name;}   // returns Product Name
+string Product::getProductName() {
+    return productName;
+}
 
-void Product::setPrice(double p) {   // set the price of the product
-    if (p>=0) {
-        Price =p;
-    }
+string Product::getCompanyName() {
+    return companyName;
 }
 
 double Product::getPrice() {
-        return Price;
+    return price;
 }
 
-void Product::displayProduct_info() {  // print Product information
-    cout <<"Product ID: "<<getProductID() <<endl;
-    cout << "Product Name: "<< getProductName() <<endl;
-    cout << "Product Price: " << Price << "OMR "<<endl;
+void Product::setProductName(string pr_name) {
+    productName = pr_name;
+}
+
+bool Product::setPrice(double pr) {
+    if (pr<0) {
+        cout << "Invalid Price!" <<endl;
+        return false;
+    }
+    price = pr;
+    return true;
+}
+
+void Product::display() const {
+    cout << left << setw(10) <<productID << setw(25) << productName << setw(20) << companyName << setw(10) << fixed << setprecision(2) << price;
+
+}
+
+bool Product::operator<(const Product& obj) const {
+     return price < obj.price;
 }
