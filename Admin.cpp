022360337173088a0a5553abdc9b77659f599f76
Meerah-AlBadi <24-0590@student.gutech.edu.om>
2026-05-17 23:32:55 +0400

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -4,50 +4,78 @@
 #include <string>
 using namespace std;
 
-Admin::Admin() : User(){
-    inventory = nullptr;}
+// Initialize static variable
+int Admin:: adminCounter =1;
 
-Admin::Admin (int id, string name, Inventory* inv) : User(id, name) {inventory =inv;}
+Admin::Admin() : User(){inventory = nullptr;}     // Default constructor
 
-void Admin::Add_Product() {
-    int ProductID, quantity;
-    string ProductName;
+Admin::Admin (int id, string name, Inventory* inv) : User(id, name) {inventory =inv;}    // Parameterized constructor
+
+
+void Admin::showMenu() {     // ShowS Admin's Menu 
+
+    cout << "\n=========== ADMIN MENU ===========\n";
+
+    cout << "1. Add Product\n";
+    cout << "2. View Inventory\n";
+    cout << "3. Update Product Stock\n";
+    cout << "4. Update Product Price\n";
+    cout << "5. Remove Product\n";
+    cout << "0. Logout\n";
+    cout << "Choose: ";
+}
+
+
+void Admin::addProduct() {
+    int quantity;
+    string ProductName, CompanyName;
     double price;
-
-    cout << "Enter Product ID: ";
-    cin >> ProductID;
 
     cout << "Enter Product Name: ";
     cin.ignore();
     getline(cin,ProductName);
 
+    cout << "Enter Product's Comapny Name: ";
+    cin.ignore();
+    getline(cin,CompanyName);
+    
     cout << "Enter Product Price: ";
     cin >> price;
 
     cout << "Enter Product Quantity: ";
     cin >> quantity;
 
-    Product product(ProductID,ProductName,price);
+    // Validation - In case pricee is 0 or -ive "or" quantity is 0 or -ive
+    if (price < 0 || quantity < 0) {
+
+        cout << "Invalid price or quantity.\n";
+        return;
+    }
+
+    Product product(ProductName,CompanyName,price);
     inventory->add_product(product,quantity);
 }
 
-void Admin::View_Inventory() {
-    inventory->display_inventory();}
+
+void Admin::viewInventory() const{
+    inventory->display_inventory();
+}
 
 
-void Admin::Update_Product_Stock() {
-    int ProductID, quantity;
+void Admin::updateProductStock() {
+    string ProductID;
+    int quantity;
 
     cout << "Enter Product ID: ";
     cin >> ProductID;
 
-    cout << "Enter quantity:";
+    cout << "Enter new quantity of Product:";
     cin >> quantity;
     
     inventory -> updateStock(ProductID,quantity);
 }
 
-void Admin::Remove_Product() {
+void Admin::removeProduct() {
     int ProductID;
 
     cout << "Enter Product ID to remove: ";
@@ -56,29 +84,12 @@ void Admin::Remove_Product() {
     inventory -> removeProduct(ProductID);
 }
 
-void Admin::Update_Product_Price() {
-    int productID;
+void Admin::updateProductPrice() {
+    string productID;
     double newPrice;
 
     cout << "Enter Product ID: ";
     cin >> productID;
 
-    Product* product = inventory->findProduct(productID);
-
-    if (product == nullptr) {
-        cout << "Product not found!" << endl;
-        return;
-    }
-
-    cout << "Enter new price: ";
-    cin >> newPrice;
-
-    if (newPrice < 0) {
-        cout << "Invalid price!" << endl;
-        return;
-    }
-
-    product->setPrice(newPrice);
-
-    cout << "Price updated successfully!" << endl;
+    inventory->updatePrice(productID,price);
 }
