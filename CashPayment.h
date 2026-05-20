a8f8ac286cbc46f9dc7a7db76c57d02229d699fe
Maryam <urmemo2311@gmail.com>
2026-05-20 22:22:04 +0400

    Update CashPayment.h

--- CashPayment.h
+++ CashPayment.h
@@ -4,18 +4,6 @@
 #include <iomanip>
 
 using namespace std;
-class Payment {
-protected:
-double amount;
-bool paid;
-public:
-    Payment(double amount) : amount(amount), paid(false) {}
-    virtual bool processPayment() = 0;
-    virtual void displayPaymentMethod() const = 0;
-    bool isPaid() const { return paid; }
-    double getAmount() const { return amount; }
-    virtual ~Payment() {}
-};
 class CashPayment : public Payment {
 public:
     CashPayment(double amount);
