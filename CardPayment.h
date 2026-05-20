0b3df43fb14843c65579bf5d45d69b02df671155
Maryam <urmemo2311@gmail.com>
2026-05-20 22:22:35 +0400

    Update CardPayment.h

--- CardPayment.h
+++ CardPayment.h
@@ -5,20 +5,6 @@
 #include <string>
 using namespace std;
 
-class Payment{
-protected:
-double amount;
-bool paid;
-public:
-Payment(double amount): amount(amount),paid(false){}
-virtual bool processPayment() = 0; 
-virtual void displayPaymentMethod() const = 0;
-
-bool isPaid() const { return paid; }
-double getAmount() const { return amount; }
-virtual ~Payment() {}
-};
-
 class CardPayment : public Payment {
 private:
 string cardNumber;
